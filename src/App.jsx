902fd84700ab72051ffd4928e823f88d293f760a
KATAMII <lydiamwaah01@gmail.com>
2024-08-22 12:54:31 +0300

    add the footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 function App() {
   
 
   return (
     <>
      <Header/>
+     <Footer/>
     </>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { FaFacebook } from "react-icons/fa6";
+import { BsTwitter } from "react-icons/bs";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import './Footer.css'
 function Footer() {
   return (
-    <div>
-      
+    <div className='footer'>
+      <p className='icon'><FaFacebook /></p>
+      <p className='icon'><BsTwitter /></p>
+      <p className='icon'><FaInstagramSquare /></p>
+      <p className='icon'><FaLinkedin /></p>
+      <p className='icon'>@ copyright <span>Katamii</span></p>
+     
     </div>
   )
 }
